79e170081f5a3b3acfae8b07b5d37b3eb68dced5
jrxgvr <dimje2@gmail.com>
2024-12-24 02:33:37 +0300

    update css plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,22 @@
+import path from "node:path";
 import { defineConfig } from 'vite'
-import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { optimizeCss, optimizeImports } from "carbon-preprocess-svelte";
-import path from "path";
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { optimizeCss } from "carbon-preprocess-svelte";
+import pluginPurgeCss from "vite-plugin-purgecss-updated-v5";
 
 // https://vite.dev/config/
-
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       includePaths: ['node_modules'],
-  //     },
-  //   },
-  // },
   resolve: {
     alias: {
       "~": path.resolve("./src"),
     },
   },
   plugins: [
-    svelte({
-      preprocess: [
-        // vitePreprocess(),
-        // optimizeImports()
-      ]
-    }), 
-    // optimizeCss()
+    svelte(), 
+    optimizeCss(),
+    pluginPurgeCss({
+      variables: true,
+    })
   ],
 })
