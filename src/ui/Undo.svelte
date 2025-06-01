9a0f3973946868e5623270cf3fac9290010d136b
jrxgvr <dimje2@gmail.com>
2025-06-01 21:41:25 +0300

    add reset and undo btn

---
+++ src/ui/Undo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Button } from "carbon-components-svelte";
+    import Undo from "carbon-icons-svelte/lib/Undo.svelte";
+  
+    const { undo } = $props()
+  </script>
+  
+  <div class="flex justify-start">
+      <Button size="small" kind="ghost" iconDescription="reset" icon={Undo} onclick={undo} />
+  </div>
